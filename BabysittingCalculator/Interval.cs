861c018609d014a1203713d60f33fe8643aa9db2
jakerutter <rutter.jacob@gmail.com>
2019-06-14 15:16:30 -0400

    added Interval class

---
+++ BabysittingCalculator/Interval.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabysittingCalculator
+{
+  public class Interval
+  {
+    private int startInterval;
+    private int endInterval;
+    private int payRate;
+
+    public int StartInterval
+    {
+      get { return startInterval; }
+    }
+    public int EndInterval
+    {
+      get { return endInterval; }
+    }
+    public int PayRate
+    {
+      get { return payRate; }
+    }
+
+    public Interval(int startInterval, int endInterval, int payRate)
+    {
+      this.startInterval = startInterval;
+      this.endInterval = endInterval;
+      this.payRate = payRate;
+    }
+
+  }
+}
